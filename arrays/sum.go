b0307b341f4fa1899af0772d3554a90b743be8fa
José Victor Zeller <josevictorzg@gmail.com>
2024-09-16 21:14:04 -0300

    done: finish 'Arrays' chapter

---
+++ arrays/sum.go
@@ -0,0 +1,32 @@
+package arrays
+
+func Sum(numbers []int) (result int) {
+	for _, number := range numbers {
+		result += number
+	}
+
+	return
+}
+
+func SumAll(numbersToSum ...[]int) (result []int) {
+
+	for _, numbers := range numbersToSum {
+		result = append(result, Sum(numbers))
+	}
+
+	return
+}
+
+func SumAllTails(numbersToSum ...[]int) (result []int) {
+
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			result = append(result, 0)
+		} else {
+			tail := numbers[1:]
+			result = append(result, Sum(tail))
+		}
+	}
+
+	return
+}
